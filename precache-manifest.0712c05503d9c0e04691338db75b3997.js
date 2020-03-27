ad96854bb491b368fdeaf0af679b7f52dbadca7f
nvjot94 <nvjot94@gmail.com>
2020-03-27 01:59:44 -0400

    Updates

---
+++ precache-manifest.0712c05503d9c0e04691338db75b3997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d69c2c74092d7fcc160b95a03a32208",
+    "url": "/gitdig/index.html"
+  },
+  {
+    "revision": "3de5c82461e304474dd0",
+    "url": "/gitdig/static/css/main.b4f77495.chunk.css"
+  },
+  {
+    "revision": "6d06a2e9a7893d6c9b01",
+    "url": "/gitdig/static/js/2.1d9750b3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/gitdig/static/js/2.1d9750b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de5c82461e304474dd0",
+    "url": "/gitdig/static/js/main.e2fdcf41.chunk.js"
+  },
+  {
+    "revision": "d299492b86f684c7292f",
+    "url": "/gitdig/static/js/runtime-main.f94c438e.js"
+  }
+]);
